274b609008c29994b10361d2266e81485324f82e
Trinetra Kumar <v-trkum@microsoft.com>
2019-07-11 03:09:42 +0530

    [TNT] : Prefernces Tab Data save

--- EmployeeConnect/Controllers/HomeController.cs
+++ EmployeeConnect/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeConnect.Helper;
 using EmployeeConnect.Models;
+using EmployeeConnect.Dialogs;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Web.Mvc;
@@ -7,11 +8,13 @@ using System.Linq;
 using System.Globalization;
 using System.Web.Script.Serialization;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace EmployeeConnect.Controllers
 {
     public class EmployeeConnectController : Controller
     {
+        public static int count = 1;
         [Route("")]
         public ActionResult Index()
         {
@@ -161,11 +164,43 @@ namespace EmployeeConnect.Controllers
             return JObject.FromObject(CardHelper.GetNewsCardbyId(NewsId));
         }
 
-
         [Route("preferences")]
         public ActionResult Preferences()
         {
             return View("Preferences");
+        }
+
+        [Route("PreferenceInDb")]
+        public void PreferenceInDb(string[] newsPrefCat, string newsTime, bool newsNotificationFlag, string newsNotifyMe, string eandtTime, string eandtNotify, bool eandtflag, string taskNotifyMe, string taskTime, bool taskNotificationFlag)
+        {
+            
+            Preference pref = new Preference();
+            pref.News = new NewsPreference[1];
+            pref.EandT = new EandtPreference[1];
+            pref.Task = new TaskPreference[1];
+            NewsPreference newsPref = new NewsPreference()
+                {
+                    SelectedCategories = newsPrefCat,
+                    NewsNotificationTime = newsTime,
+                    NewsNotifyMe = newsNotifyMe,
+                    NewsNotificationFlag = newsNotificationFlag
+            };
+            EandtPreference entPref = new EandtPreference()
+            {
+                EandTNotificationTime = eandtTime,
+                EandTNotifyMe = eandtNotify,
+                EandTNotificationFlag = eandtflag
+            };
+            TaskPreference taskPref = new TaskPreference()
+            {
+                TaskNotificationTime = taskTime,
+                TaskNotifyMe = taskNotifyMe,
+                TaskNotificationFlag = taskNotificationFlag
+            };
+            pref.News[0] = newsPref;
+            pref.EandT[0] = entPref;
+            pref.Task[0] = taskPref;
+            GetDataHelper.WritePreferences(pref);
         }
 
         [Route("policies")]
--- EmployeeConnect/Helper/GetDataHelper.cs
+++ EmployeeConnect/Helper/GetDataHelper.cs
@@ -11,6 +11,7 @@ namespace EmployeeConnect.Helper
 {
     public class GetDataHelper
     {
+        public static string userName = "";
         public static NewsModel GetNews()
         {
             string file = System.Web.Hosting.HostingEnvironment.MapPath("~/TestData/") + @"/NewsData.json";
@@ -111,6 +112,7 @@ namespace EmployeeConnect.Helper
         {
             SetPreferences pref = new SetPreferences();
             pref = (new JavaScriptSerializer().Deserialize<SetPreferences>(json));
+            userName = pref.UserName;
             return pref;
         }
 
@@ -207,6 +209,10 @@ namespace EmployeeConnect.Helper
             int i;
             EmployeeConnect.Models.UPreferences uPref = new JavaScriptSerializer().Deserialize<EmployeeConnect.Models.UPreferences>(json);
             List<EmployeeConnect.Models.Preference> list = uPref.preferences.ToList();
+            if(pref.UserName == null)
+            {
+                pref.UserName = "Abhijit Jodbhavi";
+            }
             for (i = 0; i < list.Count(); i++)
             {
                 if (list[i].UserName.Equals(pref.UserName))
