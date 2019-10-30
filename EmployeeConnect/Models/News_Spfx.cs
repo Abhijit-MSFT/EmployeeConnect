2ce536efc82b1df2f1739255e7591fe22ee0720f
Abhijit Jodhbhavi <v-abjodh@microsoft.com>
2019-10-30 16:22:29 +0530

    News and EandT spfx changes

---
+++ EmployeeConnect/Models/News_Spfx.cs
@@ -0,0 +1,67 @@
+﻿
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeConnect.Models
+{
+    public class Rootobjects
+    {
+        public string odatametadata { get; set; }
+        public Values[] value { get; set; }
+    }
+
+    public class Values
+    {
+        public string odatatype { get; set; }
+        public string odataid { get; set; }
+        public string odataetag { get; set; }
+        public string odataeditLink { get; set; }
+        public int FileSystemObjectType { get; set; }
+        public int Id { get; set; }
+        public object ServerRedirectedEmbedUri { get; set; }
+        public string ServerRedirectedEmbedUrl { get; set; }
+        public string ContentTypeId { get; set; }
+        public object ComplianceAssetId { get; set; }
+        public object WikiField { get; set; }
+        public string Title { get; set; }
+        public string CanvasContent1 { get; set; }
+        public Bannerimageurl BannerImageUrl { get; set; }
+        public string Description { get; set; }
+        public float PromotedState { get; set; }
+        public DateTime? FirstPublishedDate { get; set; }
+        public string LayoutWebpartsContent { get; set; }
+        public int[] OData__AuthorBylineId { get; set; }
+        public object OData__TopicHeader { get; set; }
+        public object OData__SPSitePageFlags { get; set; }
+        public object OData__OriginalSourceUrl { get; set; }
+        public object OData__OriginalSourceSiteId { get; set; }
+        public object OData__OriginalSourceWebId { get; set; }
+        public object OData__OriginalSourceListId { get; set; }
+        public object OData__OriginalSourceItemId { get; set; }
+        //public int ID { get; set; }
+        public DateTime Created { get; set; }
+        public int AuthorId { get; set; }
+        public DateTime Modified { get; set; }
+        public int EditorId { get; set; }
+        public object OData__CopySource { get; set; }
+        public int? CheckoutUserId { get; set; }
+        public string OData__UIVersionString { get; set; }
+        public string GUID { get; set; }
+    }
+
+    public class Bannerimageurl
+    {
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+
+}
+
+
+
+
+
---
+++ EmployeeConnect/Models/SpfxEandT.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeConnect.Models
+{
+    public class SpfxEandT
+    {
+        public string odatametadata { get; set; }
+        public Value[] value { get; set; }
+    }
+
+    public class Value
+    {
+        public string odatatype { get; set; }
+        public string odataid { get; set; }
+        public string odataetag { get; set; }
+        public string odataeditLink { get; set; }
+        public int FileSystemObjectType { get; set; }
+        public int Id { get; set; }
+        public object ServerRedirectedEmbedUri { get; set; }
+        public string ServerRedirectedEmbedUrl { get; set; }
+        public int ID { get; set; }
+        public string ContentTypeId { get; set; }
+        public string Title { get; set; }
+        public DateTime Modified { get; set; }
+        public DateTime Created { get; set; }
+        public int AuthorId { get; set; }
+        public int EditorId { get; set; }
+        public string OData__UIVersionString { get; set; }
+        public bool Attachments { get; set; }
+        public string GUID { get; set; }
+        public object ComplianceAssetId { get; set; }
+        public string Location { get; set; }
+        public Geolocation Geolocation { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public bool fAllDayEvent { get; set; }
+        public bool fRecurrence { get; set; }
+        public object ParticipantsPickerId { get; set; }
+        public object ParticipantsPickerStringId { get; set; }
+        public string Category { get; set; }
+        public string FreeBusy { get; set; }
+        public object Overbook { get; set; }
+        public Bannerurl BannerUrl { get; set; }
+        public string _1f2f { get; set; }
+    }
+
+    public class Geolocation
+    {
+        public float Altitude { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float Measure { get; set; }
+    }
+
+    public class Bannerurl
+    {
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+
+
+}
+
+
+
+
+
+
+
